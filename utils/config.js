67afa7cac6451b1baf37ff3f4d6b34a5f3801914
JYLN <14062951+JYLN@users.noreply.github.com>
2022-07-12 10:17:28 -0600

    Removed prefixes config setting due to removal of message commands

--- utils/config.js
+++ utils/config.js
@@ -3,8 +3,5 @@ module.exports = {
         red: 0xff6d6b,
         green: 0x26de81,
         yellow: 0xfeca57
-    },
-    settings: {
-        prefixes: ['<@!829220185503694849> ', '<@829220185503694849> ']
     }
 };
