5dbbca8e324bb3750680aa11672af53ea0c4e13f
JYLN <14062951+JYLN@users.noreply.github.com>
2022-07-12 12:19:46 -0600

    Re-factored to follow new naming scheme of color roles to disallow editing the name of the roles that the bot creates/manages

--- events/roleUpdate.js
+++ events/roleUpdate.js
@@ -3,12 +3,12 @@ module.exports = class RoleUpdate {
         this.client = client;
     }
 
-    run(oldRole, updated) {
-        if (oldRole.name !== updated.name) {
-            oldRole.guild.members.fetch().then(fetchMem => {
+    async run(oldRole, updated) {
+        if (oldRole.name !== updated.name && oldRole.name.includes('Custom Color')) {
+            await oldRole.guild.members.fetch().then(fetchMem => {
                 fetchMem.forEach(mem => {
-                    if (mem.user.username === oldRole.name) {
-                        updated.edit({name: mem.user.username}, 'Should not change role as it will overfill server with roles made from The Editor.');
+                    if (oldRole.name.includes(mem.user.id.substring(0,5))) {
+                        updated.edit({name: `Custom Color ${mem.user.id.substring(0,5)}`}, 'Should not change role as it will break the role functionality of The Editor');
                     }
                 });
             });
