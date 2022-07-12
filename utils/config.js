848fd21bd646e54a73c8eaf0cb072a1f0a317d43
JYLN <14062951+JYLN@users.noreply.github.com>
2022-07-12 13:31:57 -0600

    Added list of role color strings

--- utils/config.js
+++ utils/config.js
@@ -3,5 +3,31 @@ module.exports = {
         red: 0xff6d6b,
         green: 0x26de81,
         yellow: 0xfeca57
-    }
+    },
+    roleColorStrings: [
+        'Default',
+        'White',
+        'Aqua',
+        'Green',
+        'Blue',
+        'Yellow',
+        'Purple',
+        'LuminousVividPink',
+        'Fuschia',
+        'Gold',
+        'Orange',
+        'Red',
+        'Grey',
+        'Navy',
+        'DarkAqua',
+        'DarkGreen',
+        'DarkBlue',
+        'DarkPurple',
+        'DarkVividPink',
+        'DarkGold',
+        'DarkOrange',
+        'DarkRed',
+        'DarkGrey',
+        'Random'
+    ]
 };
