1e5b6d2b680a36f757041a19e23ad5ae8fbb0909
JYLN <14062951+JYLN@users.noreply.github.com>
2022-07-12 10:30:59 -0600

    Updated 'interactionCreate' event as 'slashCommands' no longer exists and has been re-factored to 'commands'

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -4,10 +4,10 @@ module.exports = class InteractionCreate {
     }
 
     async run(interaction) {
-        if (!interaction.isCommand() || !this.client.slashCommands.has(interaction.commandName)) return;
+        if (!interaction.isCommand() || !this.client.commands.has(interaction.commandName)) return;
 
         try {
-            await this.client.slashCommands.get(interaction.commandName).execute(interaction);
+            await this.client.commands.get(interaction.commandName).execute(interaction);
         } catch (error) {
             this.client.logger.error(error);
             await interaction.reply({content: 'There was an error while executing this command!', ephemeral: true});
