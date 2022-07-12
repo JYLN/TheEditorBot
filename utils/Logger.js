0de3647bf8f4015200d26517d2327bc1e9fd9b81
JYLN <14062951+JYLN@users.noreply.github.com>
2022-07-12 11:27:39 -0600

    Re-factored timestamp to helper function so that timestamp in console logging dynamically updates as intended

--- utils/Logger.js
+++ utils/Logger.js
@@ -1,25 +1,27 @@
 const Chalk = require('chalk');
 const Moment = require('moment');
 
-const timestamp = `[${Moment().format('YYYY-MM-DD HH:mm:ss')}]:`;
+const getTimestamp = () => {
+    return `[${Moment().format('YYYY-MM-DD HH:mm:ss')}]:`;
+}
 
 module.exports = {
     load(content) {
-        return console.log(`${timestamp}${Chalk.yellow(`[LOAD]`)} ${content}`);
+        return console.log(`${getTimestamp()}${Chalk.yellow(`[LOAD]`)} ${content}`);
     },
     ready(content) {
-        return console.log(`${timestamp}${Chalk.green(`[READY]`)} ${content}`);
+        return console.log(`${getTimestamp()}${Chalk.green(`[READY]`)} ${content}`);
     },
     warn(content) {
-        return console.log(`${timestamp}${Chalk.black.bgYellow(`[WARN]`)} ${content}`);
+        return console.log(`${getTimestamp()}${Chalk.black.bgYellow(`[WARN]`)} ${content}`);
     },
     error(content) {
-        return console.log(`${timestamp}${Chalk.red(`[ERR]`)} ${content.stack}`);
+        return console.log(`${getTimestamp()}${Chalk.red(`[ERR]`)} ${content.stack}`);
     },
     cmd(content) {
-        return console.log(`${timestamp}${Chalk.black.bgWhite(`[CMD]`)} ${content}`)
+        return console.log(`${getTimestamp()}${Chalk.black.bgWhite(`[CMD]`)} ${content}`)
     },
     debug(content) {
-        return console.log(`${timestamp}${Chalk.black.bgGreen(`[DEBUG]`)} ${content}`);
+        return console.log(`${getTimestamp()}${Chalk.black.bgGreen(`[DEBUG]`)} ${content}`);
     }
 }
