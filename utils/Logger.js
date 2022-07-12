5844460d5ad866956a0dab9ab7b137d4c5702000
JYLN <14062951+JYLN@users.noreply.github.com>
2022-07-12 11:15:12 -0600

    Refactored bot logging utility

--- utils/Logger.js
+++ utils/Logger.js
@@ -1,58 +1,25 @@
 const Chalk = require('chalk');
 const Moment = require('moment');
 
-class Logger {
-    static log(content, type = 'log') {
-        const timestamp = `[${Moment().format('YYYY-MM-DD HH:mm:ss')}]:`;
-        switch (type) {
-            case 'log': {
-                return console.log(`${timestamp}${Chalk.blue(`[${type.toUpperCase()}]`)} ${content}`);
-            }
-            case 'warn': {
-                return console.log(`${timestamp}${Chalk.black.bgYellow(`[${type.toUpperCase()}]`)} ${content}`);
-            }
-            case 'error': {
-                return console.log(`${timestamp}${Chalk.red(`[${type.toUpperCase()}]`)} ${content.stack}`);
-            }
-            case 'debug': {
-                return console.log(`${timestamp}${Chalk.black.bgGreen(`[${type.toUpperCase()}]`)} ${content}`);
-            }
-            case 'cmd': {
-                return console.log(`${timestamp}${Chalk.black.bgWhite(`[${type.toUpperCase()}]`)} ${content}`);
-            }
-            case 'ready': {
-                return console.log(`${timestamp}${Chalk.green(`[${type.toUpperCase()}]`)} ${content}`);
-            }
-            case 'load': {
-                return console.log(`${timestamp}${Chalk.yellow(`[${type.toUpperCase()}]`)} ${content}`);
-            }
-            default: throw new TypeError('Logger type must be either warn, debug, log, ready, cmd or error.');
-        }
-    }
+const timestamp = `[${Moment().format('YYYY-MM-DD HH:mm:ss')}]:`;
 
-    static error(content) {
-        return this.log(content, 'error');
-    }
-
-    static warn(content) {
-        return this.log(content, 'warn');
-    }
-
-    static debug(content) {
-        return this.log(content, 'debug');
-    }
-
-    static cmd(content) {
-        return this.log(content, 'cmd');
-    }
-
-    static load(content) {
-        return this.log(content, 'load');
-    }
-
-    static testLog(content) {
-        return this.log(content, 'log');
+module.exports = {
+    load(content) {
+        return console.log(`${timestamp}${Chalk.yellow(`[LOAD]`)} ${content}`);
+    },
+    ready(content) {
+        return console.log(`${timestamp}${Chalk.green(`[READY]`)} ${content}`);
+    },
+    warn(content) {
+        return console.log(`${timestamp}${Chalk.black.bgYellow(`[WARN]`)} ${content}`);
+    },
+    error(content) {
+        return console.log(`${timestamp}${Chalk.red(`[ERR]`)} ${content.stack}`);
+    },
+    cmd(content) {
+        return console.log(`${timestamp}${Chalk.black.bgWhite(`[CMD]`)} ${content}`)
+    },
+    debug(content) {
+        return console.log(`${timestamp}${Chalk.black.bgGreen(`[DEBUG]`)} ${content}`);
     }
 }
-
-module.exports = Logger;
